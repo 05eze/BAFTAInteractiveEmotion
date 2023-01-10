ef7bc3888cc07ecc57a1b1af26bf8193bb521ccb
05eze <esintim2022@elam.co.uk>
2023-01-10 11:34:35 +0000

    Working jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,21 +8,34 @@ public class CharacterController : MonoBehaviour
     public float rotation = 0.0f;
     public float camRotation = 0.0f;
     GameObject cam;
+    Rigidbody myRigidbody;
+
+
+    private bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+    
     public float rotationSpeed = 2.0f;
     public float camRotationSpeed = -1.5f;
-    Rigidbody myRigidbody;
+    
 
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
 
-        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }   
+
+            transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + transform.right * Input.GetAxis("Horizontal") * maxSpeed;
 
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
