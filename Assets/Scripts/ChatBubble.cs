be620893997a54b176517f8728a87da7c180fd76
05eze <esintim2022@elam.co.uk>
2023-01-23 11:51:23 +0000

    Text box & Scrum 2 finished

--- Assets/Scripts/ChatBubble.cs
+++ Assets/Scripts/ChatBubble.cs
@@ -20,7 +20,12 @@ public class ChatBubble : MonoBehaviour
     private void Setup(string text)
     {
         textMeshPro.SetText(text);
+		textMeshPro.ForceMeshUpdate();
+		Vector2 textSize = textMeshPro.GetRenderedValues(false);
+
+		Vector2 padding = new Vector2(4f, 2f);
+		backgroundSpriteRenderer.size = textSize + padding;
     }
 }
-//https://www.youtube.com/watch?v=K13WnNL1OYM 5:01
+//https://www.youtube.com/watch?v=K13WnNL1OYM 6:37
 //https://www.youtube.com/watch?v=LdoImzaY6M4 6:39
