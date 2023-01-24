9346c94fb8ab513746376282d9682ce10312125f
05eze <esintim2022@elam.co.uk>
2023-01-24 12:44:35 +0000

    Working dialogue system with no trigger

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteract : MonoBehaviour
 {
@@ -8,12 +9,16 @@ public class PlayerInteract : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            float interactRange = 2f;
+            float interactRange = 4f;
             Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
             foreach (Collider collider in colliderArray)
             {
                 if (collider.TryGetComponent(out NPCInteractable npcInteractable))
                     npcInteractable.Interact();
+            }
+
+            {
+                
             }
         }
     }
