9f8d0dce30aa6c8438f3374aa24919ba143274fc
05eze <esintim2022@elam.co.uk>
2023-01-25 21:40:44 +0000

    Working dialogue

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
 
     private Queue<string> sentences;
 
@@ -15,8 +20,38 @@ public class DialogueManager : MonoBehaviour
     
     public void StartDialogue (Dialogue2 dialogue)
     {
+
+        animator.SetBool("isOpen", true);
+
         Debug.Log("Starting conversation with" + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
     }
 
-    
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
+        animator.SetBool("isOpen", false);
+    }
 }
