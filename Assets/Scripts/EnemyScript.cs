35e7d2fa3369ee8439cfa32d9451183b54c4513e
05eze <esintim2022@elam.co.uk>
2023-01-29 14:13:36 +0000

    Enemy AI

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float MaxSpeed;
+    private float Speed;
+
+
+    //To see if anyone is in range
+    private Collider[] hitColliders;
+    //Laser to see if it's in line of sight
+    private RaycastHit Hit;
+
+
+    public float SightRange;
+    public float DetectionRange;
+
+
+    public Rigidbody rb;
+    public GameObject Target;
+
+
+    private bool seePlayer;
+
+    
+    void Start()
+    {
+        Speed = MaxSpeed;
+    }
+
+    
+    void Update()
+    {
+        //Detect players in range
+
+        if (!seePlayer)
+        {
+            hitColliders = Physics.OverlapSphere(transform.position, DetectionRange);
+            foreach (var HitCollider in hitColliders)
+            {
+                if(HitCollider.tag == "Player")
+                {
+                    Target = HitCollider.gameObject;
+                    seePlayer = true;
+                }
+            }
+        }
+        else
+        {
+            if(Physics.Raycast(transform.position, (Target.transform.position - transform.position), out Hit, SightRange))
+            {
+                if (Hit.collider.tag != "Player")
+                {
+                    seePlayer = false;
+                }
+                else
+                {
+                    //calculate position/direction of player
+
+                    var Heading = Target.transform.position - transform.position;
+                    var Distance = Heading.magnitude;
+                    var Direction = Heading / Distance;
+                    //move to player
+
+                    Vector3 Move = new Vector3(Direction.x * Speed , 0, Direction.z * Speed);
+                    rb.velocity = Move;
+                    transform.forward = Move;
+                }
+            }
+        }
+    }
+}
