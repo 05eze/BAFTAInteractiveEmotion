d5018d8c95c80abf7667ffb7bf8823eb742008bc
05eze <esintim2022@elam.co.uk>
2023-02-05 15:11:13 +0000

    UI additions

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+            audio.PlayScheduled(AudioSettings.dspTime + SoundToPlay.length);
+        }
+    }
+}
